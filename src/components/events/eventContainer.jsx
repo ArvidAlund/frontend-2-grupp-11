149df9ce24a50c7434b33db47949d8ad42e203fd
Arvid <arvid.alund@gmail.com>
2025-12-14 16:40:38 +0100

    event container styling uppdated

--- src/components/events/eventContainer.jsx
+++ src/components/events/eventContainer.jsx
@@ -1,34 +1,30 @@
+import { Check } from "lucide-react";
+
 const EventContainer = ({ event, onClick, type }) => {
 
   return (
     <li
-      className={`rounded-lg shadow-md p-4 mb-4 transition text-black select-none cursor-pointer
-        bg-white hover:shadow-lg hover:bg-neutral-300`
+      className={`rounded-xl shadow-md p-6 mb-4 transition text-white select-none cursor-pointer
+        bg-[rgb(19,41,54)] border border-transparent hover:border-green-300 grid grid-cols-[2fr_1fr] min-h-40 max-w-[90%]`
       }
       onClick={() => onClick(event)}
     >
-      <div className="flex justify-between items-center mb-2">
+      <div className="flex flex-col items-start gap-2">
         <h3 className="text-lg font-semibold">{event.title}</h3>
-        <span
-          className={`text-xs px-2 py-1 rounded-full 
-            ${type === "past" ? "bg-neutral-300 text-neutral-600" : "bg-green-100 text-green-700"}`
-          }
-        >
-          {type === "past" ? "Avslutat" : "Kommande"}
-        </span>
+        <p>{event.description}</p>
       </div>
 
-      <p className="text-sm mb-3">{event.description}</p>
+      <div className="text-neutral-500 flex flex-col justify-between items-end gap-2">
+        <p className="font-semibold">Datum: {new Date(event.startDate).toLocaleDateString("sv-SE", { year: 'numeric', month: 'short', day: 'numeric' })} - <br /> {new Date(event.endDate).toLocaleDateString("sv-SE", { year: 'numeric', month: 'short', day: 'numeric' })}</p>
+        <p className="font-semibold">Tid: {new Date(event.startDate).toLocaleTimeString("sv-SE", { hour: '2-digit', minute: '2-digit' })} - {new Date(event.endDate).toLocaleTimeString("sv-SE", { hour: '2-digit', minute: '2-digit' })}</p>
+        <p className="font-semibold">Plats:</p>
 
-      <div className="text-sm space-y-1">
-        <p>
-          <span className="font-medium">Från:</span>{" "}
-          {new Date(event.startDate).toLocaleString()}
-        </p>
-        <p>
-          <span className="font-medium">Till:</span>{" "}
-          {new Date(event.endDate).toLocaleString()}
-        </p>
+        <div className="flex items-center text-white gap-2 mt-4">
+          <p>Status:</p>
+          <span className={`h-3 aspect-square rounded-full ring-2 ${type === "coming" ? "ring-green-500" : "ring-neutral-500"} flex items-center justify-center overflow-hidden`}>
+            {type === "coming" ? <Check className="w-3 h-3 bg-green-500 rounded-full" /> : null}
+          </span>
+        </div>
       </div>
     </li>
   );
